ede9f28084c3219d3a8e32c94df8453814a50185
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-29 21:39:18 +0600

    system update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
+import { useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,33 +9,57 @@ import {
   Link
 } from "react-router-dom";
 import TeamDetails from './Components/TeamDetails/TeamDetails';
+import Team from './Components/Team/Team';
 
 
 function App() {
+
+  const [teams,setTeams] = useState([]);
+  useEffect(()=>{
+
+      fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_leagues.php?c=England&s=Soccer')
+      .then(res=>res.json())
+      .then(data=>setTeams(data.countrys))
+      // .then(data=>console.log(data.countrys))
+  },)
   return (
-    <div className="App" >
+    <div className="just">
 
-<Router>
-     
-   
-        <Switch>
-       
-          <Route path="/">
-            <Home />
-          </Route>
+<div className="homePageImage"  >
+                <img className="card-img-top" style={{height:'400px'}} src="https://www.thesportsdb.com/images/media/team/stadium/w1anwa1588432105.jpg" alt="" />
+                <h1 className="text-image" style={{  }}>All Star League</h1>
+            </div>
 
-          
-          <Route path="/post/:idLeague">
-            <TeamDetails></TeamDetails>
-          </Route>
-
-          
-        </Switch>
+<div className="App" >
       
-    </Router>
 
-     
+      <Router>
+           
+      
+              <Switch>
+             
+              <Route exact path="/">
+                 { teams.map(tm=><Team
+      
+                           team={tm}>
+      
+                           </Team>)}
+                </Route>
+      
+                
+                <Route path="/post/:idLeague">
+                  <TeamDetails></TeamDetails>
+                </Route>
+      
+                
+              </Switch>
+            
+          </Router>
+      
+           
+          </div>
     </div>
+    
   );
 }
 
