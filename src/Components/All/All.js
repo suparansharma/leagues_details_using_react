58c7462a2845c7480fe4a232a8cdbc7e323cd0e8
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-30 23:42:47 +0600

    all wokr with out css

--- src/Components/All/All.js
+++ src/Components/All/All.js
@@ -3,11 +3,14 @@ import {Card} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
 import male from '../../images/male.png';
 import female from '../../images/female.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFlag, faFutbol, faMars } from '@fortawesome/free-solid-svg-icons';
+import './All.css';
 
 
 const All = (props) => {
 
-    const {strLeague, intFormedYear,strLeagueAlternate,strGender}=props.detail;
+    const {strLeague, intFormedYear,strLeagueAlternate,strGender,strCountry,strSport,strBanner,strDescriptionEN}=props.detail;
 
     let imgSrc =``;
     if (strGender === "Male") {
@@ -18,18 +21,29 @@ const All = (props) => {
     }
     return (
         <div>
-            <img src={imgSrc} alt="" />
-          <Card>
-  
-  <Card.Body>
-    <Card.Title>{strLeagueAlternate}</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+          
+          <div className="row">
+                <img className="card-img-top" src={strBanner} alt="" />
+            </div>
+            <div className="row m-4 legueDetailsHeader rounded">
+                <div id="first" className="col-md text-left p-5">
+                    <h2>{strLeague}</h2>
+                   
+                    <p> <FontAwesomeIcon icon={faFlag} /> Country: {strCountry}</p>
+                    <p> <FontAwesomeIcon icon={faFutbol} /> Sport type: {strSport}</p>
+                    <p> <FontAwesomeIcon icon={faMars} /> Gender: {strGender}</p>
 
+
+                </div>
+                <div id="second" className="col-md text-right p-5 ">
+                    <img className="card-img-top" src={imgSrc} alt="image not found" />
+                </div>
+            </div>
+            <div className="row m-3 p-2 text-left">
+                <p>{strDescriptionEN}</p>
+            </div>
+
+            
         </div>
     );
 };
